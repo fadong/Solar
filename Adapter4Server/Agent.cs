975d5e9e173a0e69ddb34faf90c2c4cf4ae89300
fadong <jaekyoon.lee@gmail.com>
2014-02-04 22:45:24 +0900

    추가

---
+++ Adapter4Server/Agent.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Security.Claims;
+using System.Net;
+using System.Net.Sockets;
+using CommonInterface;
+using CommonLib;
+
+namespace Adapter4Server {
+    public class Agent : IClientServiceCallback {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="pw"></param>
+        /// <param name="isLocalhost"></param>
+        /// <returns></returns>
+        public List<Claim> Connect(string id, string pw, bool isLocalhost) {
+            try {
+                ClientInfo cinfo = new ClientInfo();
+                var ip = Dns.GetHostAddresses(Dns.GetHostName()).Where(k => k.AddressFamily == AddressFamily.InterNetwork).First();
+                cinfo.IP = ip.ToString();
+                cinfo.UserId = id;
+                cinfo.PassWord = pw;
+                cinfo.UserName = Environment.UserName;
+                cinfo.DomainName = Environment.UserDomainName;
+                cinfo.OSVersion = Environment.OSVersion.VersionString;
+                cinfo.OSVersion += Environment.Is64BitOperatingSystem ? "64it" : "32bit";
+                cinfo.SysDir = Environment.SystemDirectory;
+                cinfo.CurrDir = Environment.CurrentDirectory;
+                cinfo.RequestTime = DateTime.Now;
+                cinfo.ConnectedTime = DateTime.Now;
+                return RemoteConnect(cinfo, isLocalhost);
+            } catch (Exception) {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cinfo"></param>
+        /// <param name="isLocalhost"></param>
+        /// <returns></returns>
+        private List<Claim> RemoteConnect(ClientInfo cinfo, bool isLocalhost) {
+            try {
+                List<Claim> claims = new List<Claim>();
+
+
+                return claims;
+            } catch (Exception) {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ CommonInterface/DataContract.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+namespace CommonInterface
+{
+    [Serializable]
+    [DataContract]
+    public class ClientInfo {
+        [DataMember]
+        public string SessionId { get; set; }
+
+        [DataMember]
+        public string Identifer { get; set; }
+
+        [DataMember]
+        public string IP { get; set; }
+
+        [DataMember]
+        public string UserId { get; set; }
+
+        [DataMember]
+        public string PassWord { get; set; }
+
+        [DataMember]
+        public DateTime RequestTime { get; set; }
+
+        [DataMember]
+        public DateTime ConnectedTime { get; set; }
+
+        [DataMember]
+        public int Port { get; set; }
+
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public string DomainName { get; set; }
+
+        [DataMember]
+        public string SysDir { get; set; }
+
+        [DataMember]
+        public string CurrDir { get; set; }
+
+        [DataMember]
+        public string OSVersion { get; set; }
+
+        [DataMember]
+        public int ProcessNum { get; set; }
+
+        public override string ToString() {
+            return string.Format("{0} [{1}]", UserId, IP);
+        }
+    }
+}
