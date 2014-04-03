cd7afc4dc5e32593dc8a7389b5d7bfb4d934d893
fadong <jaekyoon.lee@gmail.com>
2014-04-04 01:24:28 +0900

    fpML + 확장상품

---
+++ CommonLib/Legs/ReturnLeg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Com.Fadong.CommonLib.Legs {
+    public partial class ReturnLeg : UserControl {
+        public ReturnLeg() {
+            InitializeComponent();
+        }
+    }
+}
