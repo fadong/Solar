d0010005c0168e6736d618a4104507e0c1002448
fadong <jaekyoon.lee@gmail.com>
2014-04-10 08:55:39 +0900

    nameSpace Changed!!

---
+++ ZClient/UCtlComps/ProdELS_UndInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Com.Fadong.ZClient.UCtlComps {
+    public partial class ProdELS_UndInfo : UserControl {
+        public ProdELS_UndInfo() {
+            InitializeComponent();
+        }
+    }
+}
